a16ce6ee8f70b505b2d8bc2ad8ba2d5102112d28
VIGNESH V <130889672+vignesh-camkie@users.noreply.github.com>
2025-03-04 00:08:07 +0530

    Add files via upload

---
+++ GCD of 2 Numbers Using Euclidean Algorithm .c
@@ -0,0 +1,22 @@
+// Find the GCD of Two Numbers using Euclidean Algorithm 
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    int result = gcd(num1, num2);
+    printf("GCD of %d and %d is: %d\n", num1, num2, result);
+
+    return 0;
+}
